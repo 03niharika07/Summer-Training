f9feb3203db314ab58465565f4853059de3e1dc3
03niharika07 <niharika20052004@gmail.com>
2026-07-05 16:02:05 +0530

    New

---
+++ Python Fundamentals/2.DataTypes.py
@@ -0,0 +1,48 @@
+# DATA TYPES
+
+# 1. Integer 
+print(8)
+print(1e308)        # 1*10^308, max int value it can handle
+print(1e309)        # 1*10^309, show inf (infinite)
+print('\n')
+
+# 2. Decimal/Float
+print(8.76)
+print(1.7e308)      # 1.7*10^308
+print(1.7e309)      # 1.7*10^309
+print('\n')
+
+# 3. Boolean
+print(True)         # capital T, True
+print(False)        # capital F, False
+print('\n')
+
+# 4. Text/String
+print('Hello World')
+print('\n')
+
+# 5. Complex Data Type
+print(5+6j)
+print('\n')
+
+# 6. List
+print([1,2,3,4,5])
+print('\n')
+
+# 7. Tuple
+print((1,2,23))
+print('\n')
+
+# 8. Sets
+print({1,2,34,5})
+print('\n')
+
+# 9. Dictionary (key-value pairs)
+print({'Name':'Niharika','Course':'B.Tech'})
+print('\n')
+
+
+# type function - tells the type of the data
+print(type([1,2,3]))
+print(type(8))
+print(type({1,2,3}))
